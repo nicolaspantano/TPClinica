930b83517039a923904a3bdd10eed02915ad46a5
nicolaspantano <nicolaspantano_757@hotmail.com>
2021-05-15 23:23:11 -0300

    commit1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'',component:HomeComponent},
+  {path:'registro',component:RegistroComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
