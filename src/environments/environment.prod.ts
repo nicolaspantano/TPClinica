366563e1cac87b92e566d47e424056c83991d9eb
nicolaspantano <nicolaspantano_757@hotmail.com>
2021-05-20 08:48:13 -0300

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBsv5h3PczvIxinvKDo65IsjEIhDFDUU9I",
+    authDomain: "tpclinica-6668b.firebaseapp.com",
+    projectId: "tpclinica-6668b",
+    storageBucket: "tpclinica-6668b.appspot.com",
+    messagingSenderId: "891648176626",
+    appId: "1:891648176626:web:2b94a52fd8ffde3a013b6b"
+  }
+
 };
